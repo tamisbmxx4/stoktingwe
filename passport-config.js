250e1cf02a1c6265b028eb2c4813d7e17a581613
tamisbmxx4 <muchtamiltamyis@gmail.com>
2023-06-16 15:42:31 +0700

    first commit

--- passport-config.js
+++ passport-config.js
@@ -5,7 +5,7 @@ function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
     if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
+      return done(null, false, { message: 'Tidak ada pengguna dengan email ini' })
     }
 
     try {
@@ -16,7 +16,7 @@ function initialize(passport, getUserByEmail, getUserById) {
         console.log(`password =${password}`)
         console.log(`user.password =${user.password}`)
         
-        return done(null, false, { message: 'Password incorrect' })
+        return done(null, false, { message: 'Password Atau Kata Sandi Salah' })
       }
     } catch (e) {
       return done(e)
